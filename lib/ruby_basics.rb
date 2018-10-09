45e23f9eadaea70566ac7399048edaee28f15289
Danny Wu <dannyw3292@hstat.org>
2018-10-09 16:53:21 +0000

    complete assignment

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,23 @@
-def division
-    
+def division(num1 = 10, num2 = 2)
+    num1/num2
 end
 
-def argue
-  
+def argue(test1)
+  return test1
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
-def return_a_value
-    
+def return_a_value(value = "Nice")
+    return value
 end
 
-def pizza_party
-    
+def pizza_party(parameter = "cheese")
+    return parameter
 end
 
-def whisper
-    
+def whisper(user)
+    return user.downcase
 end
